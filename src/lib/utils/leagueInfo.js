9032f3ff1e1d6cea49a327f75e026b60c8be152e
boatjack <155322165+boatjack@users.noreply.github.com>
2024-07-21 18:17:32 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "992910658662424576"; // your league ID
+export const leagueID = "1120916507824679968"; // your league ID
 export const leagueName = "League of Fantasy Legends"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
